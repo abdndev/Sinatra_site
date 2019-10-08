a3985ac0b3d2b46c36e24e412b540294e2805d28
ABDNdev <igor@abidin.ru>
2019-10-08 20:53:50 +0400

    Display table with user information

--- app.rb
+++ app.rb
@@ -91,6 +91,13 @@ post '/contacts' do
 
 end
 
+
 get '/showusers' do
-  erb :showusers
+	db = get_db
+	db.results_as_hash = true
+	@results = db.execute 'select * from Users order by id desc'
+
+  	erb :showusers
 end
+
+
